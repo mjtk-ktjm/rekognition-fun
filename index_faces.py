a8cab9702e3549341d6ca27bf80bdfcd7cdc624a
Mathew Kelly <mjtk.ktjm@gmail.com>
2019-09-06 04:59:18 +0000

    Improve index_faces logging and externalImageId

--- index_faces.py
+++ index_faces.py
@@ -19,11 +19,12 @@ for file_name in all_objects[100:]:
         name_only, _ = file_name.split(".")
         name_only = name_only[:-1]
         for i in range(1,8):
-            this_name = "faces/" + name_only + str(i) + ".jpg"
-            print(this_name)
+            external_image_id = name_only + str(i)
+            this_name = "faces/" + external_image_id + ".jpg"
+            print("Indexing face ".format(external_image_id, this_name))
             response=rek_client.index_faces(CollectionId=collection_id,
                                 Image={'S3Object':{'Bucket':bucket_name,'Name':this_name}},
-                                ExternalImageId=name_only,
+                                ExternalImageId=external_image_id,
                                 DetectionAttributes=["ALL"],
                                 MaxFaces=1,
                                 QualityFilter="AUTO",
